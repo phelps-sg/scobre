ccd3217f59568d5c52a9583d4eedd6bf39ae9b0d
sphelps <phelps.sg@gmail.com>
2013-09-09 22:10:01 +0100

    further normalisation

--- lse-data/sql/createEvents.sql
+++ lse-data/sql/createEvents.sql
@@ -7,7 +7,12 @@ CREATE TABLE events (
 	order_code VARCHAR(10) REFERENCES orders(order_code),
 	transaction_id BIGINT UNSIGNED REFERENCES transactions(transaction_id),
 	message_sequence_number INT UNSIGNED NOT NULL,
-	time_stamp BIGINT UNSIGNED NOT NULL
+	time_stamp BIGINT UNSIGNED NOT NULL,
+	ti_code VARCHAR(12) NOT NULL,
+	market_segment_code VARCHAR(4) NOT NULL,
+	country_of_register VARCHAR(2) NOT NULL,
+	currency_code VARCHAR(3) NOT NULL
 );
 
 CREATE INDEX id_events_time ON events(time_stamp, message_sequence_number) USING BTREE;
+CREATE INDEX id_events_ti_code ON events(ti_code) USING HASH;
--- lse-data/sql/createTradeReports.sql
+++ lse-data/sql/createTradeReports.sql
@@ -3,19 +3,43 @@ USE lse_tickdata;
 CREATE TABLE trade_reports_raw (
 	message_sequence_number BIGINT UNSIGNED NOT NULL,
 	ti_code VARCHAR(12) NOT NULL,
-	market_segment_code varchar(4) NOT NULL,
-	CountryOfRegister varchar(2) NOT NULL,
-	CurrencyCode varchar(3) NOT NULL,
-	TradeCode varchar(50) NOT NULL,
-	TradePrice decimal(18, 8) NOT NULL,
-	TradeSize decimal(12, 0) NOT NULL,
-	TradeDate varchar(8) NOT NULL,
-	TradeTime varchar(15) NOT NULL,
-	BroadcastUpdateAction varchar(1) NOT NULL,
-	TradeTypeInd varchar(2) NOT NULL,
-	TradeTimeInd varchar(1) NOT NULL,
-	BargainConditions varchar(1) NOT NULL,
-	ConvertedPriceInd varchar(1) NOT NULL,
-	PublicationDate varchar(8) NOT NULL,
-	PublicationTime varchar(8) NOT NULL
+	market_segment_code VARCHAR(4) NOT NULL,
+	country_of_register VARCHAR(2) NOT NULL,
+	currency_code VARCHAR(3) NOT NULL,
+	trade_code VARCHAR(50) NOT NULL,
+	trade_price DECIMAL(18, 8) NOT NULL,
+	trade_size DECIMAL(12, 0) NOT NULL,
+	trade_date VARCHAR(8) NOT NULL,
+	trade_time VARCHAR(15) NOT NULL,
+	broadcast_update_action VARCHAR(1) NOT NULL,
+	trade_type_ind VARCHAR(2) NOT NULL,
+	trade_time_ind VARCHAR(1) NOT NULL,
+	bargain_conditions VARCHAR(1) NOT NULL,
+	converted_price_ind VARCHAR(1) NOT NULL,
+	publication_date VARCHAR(8) NOT NULL,
+	publication_time VARCHAR(15) NOT NULL
 );
+
+CREATE TABLE transactions (
+	transaction_id BIGINT UNSIGNED PRIMARY KEY,
+	message_sequence_number BIGINT UNSIGNED NOT NULL,
+-- 	ti_code VARCHAR(12) NOT NULL,
+-- 	market_segment_code VARCHAR(4) NOT NULL,
+-- 	country_of_register VARCHAR(2) NOT NULL,
+-- 	currency_code VARCHAR(3) NOT NULL,
+	trade_code VARCHAR(50) NOT NULL,
+	trade_price DECIMAL(18, 8) NOT NULL,
+	trade_size DECIMAL(12, 0) NOT NULL,
+	trade_time_stamp BIGINT UNSIGNED NOT NULL,
+	broadcast_update_action VARCHAR(1) NOT NULL,
+	trade_type_ind VARCHAR(2) NOT NULL,
+	trade_time_ind VARCHAR(1) NOT NULL,
+	bargain_conditions VARCHAR(1) NOT NULL,
+	converted_price_ind VARCHAR(1) NOT NULL,
+	publication_time_stamp BIGINT UNSIGNED NOT NULL
+)
+
+CREATE INDEX id_transactions_time ON transactions(trade_time_stamp, message_sequence_number);
+CREATE INDEX id_transactions_publication ON transactions(publication_time_stamp, message_sequence_number);
+CREATE INDEX id_transactions_trade_size ON transactions(trade_size);
+CREATE INDEX id_transactions_trade_price ON transactions(trade_price) USING HASH;
