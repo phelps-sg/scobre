c357e2e6eb288906a6793b08845e1982e6f4ff42
sphelps <phelps.sg@gmail.com>
2013-09-09 21:22:40 +0100

    tested

--- lse-data/sql/createEvents.sql
+++ lse-data/sql/createEvents.sql
@@ -5,9 +5,9 @@ CREATE TABLE events (
 	event_type ENUM('order_submitted', 'order_revised', 'transaction') NOT NULL,
 	order_history_event_id BIGINT UNSIGNED REFERENCES order_history(event_id),
 	order_code VARCHAR(10) REFERENCES orders(order_code),
-	transaction_id BIGINT UNSIGNED REFERENCES transactions(transaction_id)
-	message_sequence_number UNSIGNED INT NOT NULL,
+	transaction_id BIGINT UNSIGNED REFERENCES transactions(transaction_id),
+	message_sequence_number INT UNSIGNED NOT NULL,
 	time_stamp BIGINT UNSIGNED NOT NULL
 );
 
-CREATE INDEX id_events_time ON events(timestamp, message_sequence_number) USING BTREE;
+CREATE INDEX id_events_time ON events(time_stamp, message_sequence_number) USING BTREE;
