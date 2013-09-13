153ee271c0ecc1f325fda0a7d3fee8852b06dd43
sphelps <phelps.sg@gmail.com>
2013-09-13 15:07:09 +0100

    added

---
+++ lse-data/src/org/ccfea/tickdata/OrderReplay.scala
@@ -0,0 +1,31 @@
+package org.ccfea.tickdata
+
+import net.sourceforge.jasa.market._
+import scala.slick.driver.MySQLDriver.simple._
+import java.text.SimpleDateFormat
+import RelationalTables._
+
+// Use the implicit threadLocalSession
+import Database.threadLocalSession
+
+ object OrderReplay {
+
+
+  def main(args: Array[String]) {
+    
+		val host = args(0)
+		val user = args(1)
+		val password = args(2)
+		val url = 
+		  "jdbc:mysql://%s/lse_tickdata?user=%s&password=%s".format(
+				  										host, user, password)
+
+		Database.forURL(url, driver="com.mysql.jdbc.Driver") withSession {
+		  
+			for(event <- Query(events)) {
+				println(event)
+			}
+
+		}
+  }
+}
