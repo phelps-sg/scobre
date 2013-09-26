05378d40d194ccd0b5c01ed93be966b723901a46
Steve Phelps <phelps.sg@gmail.com>
2013-09-26 20:37:27 +0100

    Fixed order fill

--- lse-data/src/org/ccfea/tickdata/OrderReplay.scala
+++ lse-data/src/org/ccfea/tickdata/OrderReplay.scala
@@ -22,13 +22,13 @@ import java.awt.BorderLayout
 class MarketState {
 
   val book = new FourHeapOrderBook()
-  var lastChanged: Option[SimulationTime] = None
-
   val orderMap = collection.mutable.Map[String, Order]()
+  var time: Option[SimulationTime] = None
+  var lastTransactionPrice: Option[Double] = None
 
-  def processEvent(ev: Event) = {
+   def processEvent(ev: Event) = {
 
-    lastChanged = Some(new SimulationTime(ev.timeStamp))
+    time = Some(new SimulationTime(ev.timeStamp))
 
     ev match {
 
@@ -47,9 +47,9 @@ class MarketState {
                   order.setQuantity(aggregateSize.toInt)
                   order.setAgent(new SimpleTradingAgent())
                   order.setIsBid(buySellInd equals "B")
-                  order.setTimeStamp(lastChanged.get)
+                  order.setTimeStamp(time.get)
                   if (orderMap.contains(orderCode)) {
-                    println("Order revision to " + orderMap(orderCode))
+//                    println("Order revision to " + orderMap(orderCode))
                   }
                   orderMap(orderCode) = order
                   if (marketMechanismType equals "LO") {
@@ -68,13 +68,13 @@ class MarketState {
                       None, None, None, None, None)
 
           => {
-                  println("Removing order " + orderCode)
+//                  println("Removing order " + orderCode)
                   if (orderMap.contains(orderCode)) {
                     val removedOrder = orderMap(orderCode)
-                    println("Removed " + removedOrder)
+//                    println("Removed " + removedOrder)
                     book.remove(removedOrder)
                   } else {
-                    println("Unable to find order " + orderCode)
+//                    println("Unable to find order " + orderCode)
                   }
 
           }
@@ -88,16 +88,29 @@ class MarketState {
                   matchingOrderCode, resultingTradeCode, None, None, None)
 
           => {
-            println("Order matched " + orderCode)
+//            println("Order matched " + orderCode)
             if (orderMap.contains(orderCode)) {
               val order = orderMap(orderCode)
-              println("Found order " + order)
+//              println("Found order " + order)
               book.remove(order)
             } else {
-              println("Unable to find order " + orderCode)
+//              println("Unable to find order " + orderCode)
             }
-
           }
+
+          case Event(id, EventType.Transaction,
+              messageSequenceNumber, timeStamp,
+              tiCode, marketSegmentCode,
+              None, None, None, None,
+              Some(tradeSize), Some(broadcastUpdateAction),
+              None, None, Some(tradePrice), None,
+              None, None,
+              tradeCode, Some(tradeTimeInd), Some(convertedPriceInd))
+
+           => {
+              lastTransactionPrice = Some(tradePrice.toDouble)
+          }
+
           case _ => // println("Do not know how to process " + ev)
     }
   }
@@ -137,6 +150,18 @@ class MarketState {
     }
   }
 
+  def price(level: Int, orders: Seq[Order]): Option[Double] = {
+    if (level < orders.length) {
+      Some(orders.sorted.get(level).getPrice)
+    } else {
+      None
+    }
+  }
+
+  //TODO cache
+  def bidPrice(level: Int) = price(level, book.getUnmatchedBids)
+  def askPrice(level: Int) = price(level, book.getUnmatchedAsks)
+
 }
 
 class MarketStateWithGUI extends MarketState {
@@ -152,7 +177,7 @@ class MarketStateWithGUI extends MarketState {
 }
 
 class OrderBookView(val market: MarketState) {
-  val df = new SimpleDateFormat("hh:mm:ss dd/MM yyyy")
+  val df = new SimpleDateFormat("HH:mm:ss:SSSS dd/MM yyyy")
   val auctioneer = new ContinuousDoubleAuctioneer()
   auctioneer.setOrderBook(market.book)
   val orderBookView = new net.sourceforge.jasa.view.OrderBookView()
@@ -172,13 +197,13 @@ class OrderBookView(val market: MarketState) {
       def run() = {
         orderBookView.update();
         orderBookView.notifyTableChanged()
-        timeLabel.setText(df.format(new java.util.Date(market.lastChanged.get.getTicks)))
+        timeLabel.setText(df.format(new java.util.Date(market.time.get.getTicks)))
       }
     })
   }
 }
 
-class OrderFlow(val orders: Seq[Event], val market: MarketState = new MarketStateWithGUI()) {
+class OrderFlow(val orders: Seq[Event], val market: MarketState = new MarketState()) {
 
   def map[B](f: MarketState => B): Seq[B] = {
     orders.map(ev => {
@@ -202,10 +227,11 @@ object OrderReplay {
       val timeSeries =
         for {
           market <- new OrderFlow(allEventsByTime.list)
-        } yield (market.lastChanged, market.midPrice)
+        } yield (market.time, market.midPrice)
 
       for ((t, price) <- timeSeries) {
-        println(t.get.getTicks() + "\t" + (price match {
+//          println(t.get.getTicks + "\t" + price)
+        println(t.get.getTicks + "\t" + (price match {
           case Some(p) => p.toString()
           case None => "NaN"
         }))
