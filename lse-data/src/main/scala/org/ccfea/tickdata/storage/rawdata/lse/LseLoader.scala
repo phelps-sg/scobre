98ab39ab79bb837be11c65d7903b2b13c33aa708
sphelps <sphelps@sphelps.net>
2014-10-07 14:45:55 +0100

    moved LSE-specific functionality from DataLoader to LseLoader

--- lse-data/src/main/scala/org/ccfea/tickdata/storage/rawdata/lse/LseLoader.scala
+++ lse-data/src/main/scala/org/ccfea/tickdata/storage/rawdata/lse/LseLoader.scala
@@ -1,6 +1,8 @@
 package org.ccfea.tickdata.storage.rawdata.lse
 
 import grizzled.slf4j.Logger
+import org.ccfea.tickdata.event.{EventType, Event}
+import org.ccfea.tickdata.order.{TradeDirection, MarketMechanismType}
 import org.ccfea.tickdata.storage.rawdata.HasDateTime
 
 /**
@@ -114,5 +116,92 @@ trait LseLoader {
     }
   }
 
+
+ /**
+   * Convert a row of one of the three LSE tables (order_history, order_detail, trade_reports)
+   * into an Event object which represents a tick.
+   *
+   * @param rawEvent  A tuple representation of the raw data in the current row being parsed.
+   * @return  A tick whose eventType represents the type of event that has occurred.
+   */
+  def parseEvent(rawEvent: HasDateTime): Event = {
+
+    logger.debug("Raw event = " + rawEvent)
+
+    implicit def marketMechanismTypeToEnum(marketMechanismType: String): MarketMechanismType.Value = {
+      marketMechanismType match {
+        case "LO" => MarketMechanismType.LimitOrder
+        case "MO" => MarketMechanismType.MarketOrder
+        case _ =>    MarketMechanismType.Other
+      }
+    }
+
+    implicit def orderActionTypeToEventType(orderActionType: String): EventType.Value = {
+      orderActionType match {
+        case "D" => EventType.OrderDeleted
+        case "E" => EventType.OrderExpired
+        case "P" => EventType.OrderMatched
+        case "M" => EventType.OrderFilled
+        case "T" => EventType.TransactionLimit
+      }
+    }
+
+    implicit def buySellIndToTradeDirection(buySellInd: String): TradeDirection.Value = {
+      buySellInd match {
+        case "B" => TradeDirection.Buy
+        case "S" => TradeDirection.Sell
+      }
+    }
+
+    rawEvent match {
+
+      case OrderHistoryRaw(orderCode, orderActionType, matchingOrderCode,
+                              tradeSize, tradeCode, tiCode, countryOfRegister,
+                              currencyCode, marketSegmentCode,
+                              aggregateSize, buySellInd,
+                              marketMechanismType,
+                              messageSequenceNumber, date, time) =>
+
+          Event(None, orderActionType, messageSequenceNumber,
+            rawEvent.timeStamp, tiCode, marketSegmentCode, currencyCode,
+            Some(marketMechanismType), Some(aggregateSize), Some(buySellInd),
+            Some(orderCode), tradeSize, None, None, None, None, None,
+            matchingOrderCode, tradeCode,
+            None, None, None)
+
+
+      case OrderDetailRaw(orderCode, marketSegmentCode, marketSectorCode,
+                          tiCode, countryOfRegister, currencyCode,
+                          participantCode, buySellInd, marketMechanismGroup,
+                          marketMechanismType, price, aggregateSize,
+                          singleFillInd, broadcastUpdateAction, date, time,
+                          messageSequenceNumber) =>
+
+          Event(None, EventType.OrderSubmitted, messageSequenceNumber,
+            rawEvent.timeStamp, tiCode, marketSegmentCode, currencyCode,
+            Some(marketMechanismType), Some(aggregateSize), Some(buySellInd),
+            Some(orderCode), None, Some(broadcastUpdateAction),
+            Some(marketSectorCode), Some(marketMechanismGroup), Some(price), Some(singleFillInd),
+            None, None,
+            None, None, None)
+
+      case TradeReportRaw(messageSequenceNumber, tiCode, marketSegmentCode,
+                          countryOfRegister, currencyCode, tradeCode,
+                          tradePrice, tradeSize, date, time,
+                          broadcastUpdateAction, tradeTypeInd,
+                          tradeTimeInd, bargainConditions, convertedPriceInd,
+                          publicationDate, publicationTime) =>
+
+          Event(None, EventType.Transaction,
+            messageSequenceNumber, rawEvent.timeStamp,
+            tiCode, marketSegmentCode, currencyCode,
+            None, None, None, None,
+            Some(tradeSize), Some(broadcastUpdateAction),
+            None, None, tradePrice, None,
+            None, None,
+            tradeCode, Some(tradeTimeInd), Some(convertedPriceInd))
+    }
+
+  }
 
 }
