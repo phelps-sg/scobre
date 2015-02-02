62ada576aad4e26b4f1195e5e24b659f3dbc4053
phelps-sg <sphelps@sphelps.net>
2015-02-02 09:51:28 +0000

    scaladoc updates

--- lse-data/src/main/scala/org/ccfea/tickdata/storage/hbase/HBaseInserter.scala
+++ lse-data/src/main/scala/org/ccfea/tickdata/storage/hbase/HBaseInserter.scala
@@ -5,12 +5,16 @@ import collection.JavaConversions._
 import org.ccfea.tickdata.event.{EventType, Event}
 
 /**
- * Store events in Apache HBase.
+ * Store a sequence of {@link org.ccfea.tickdata.event.Event} objects in an Apache HBase table.
  *
  * (c) Steve Phelps 2013
  */
 trait HBaseInserter extends HBaseEventConverter {
 
+  /**
+   * The column names of the events table.  These also correspond with the
+   * attributes of {@link org.ccfea.tickdata.event.Event}.
+   */
   val fields: List[String] = List("eventType", "marketSegmentCode", "currencyCode", "marketMechanismType",
     "aggregateSize", "tradeDirection", "orderCode", "tradeSize", "broadcastUpdateAction", "marketSectorCode",
     "marketMechanismGroup", "price", "singleFillInd", "matchingOrderCode", "resultingTradeCode", "tradeCode",
@@ -31,6 +35,13 @@ trait HBaseInserter extends HBaseEventConverter {
     }
   }
 
+  /**
+   * Convert a single {@link org.ccfea.tickdata.event.Event} object into an
+   * HBase Put object.
+   *
+   * @param event   A single tick.
+   * @return        An HBase Put object which can be inserted into the database.
+   */
   implicit def convert(event: Event): Put = {
     implicit val timeStamp = event.timeStamp
     implicit val put: Put = new Put(getKey(event))
@@ -39,12 +50,17 @@ trait HBaseInserter extends HBaseEventConverter {
     put
   }
 
+  /**
+   * Insert a sequence of ticks into the database.
+   *
+   * @param parsedEvents    A sequence of ticks.
+   * @return                The total number of ticks successfully inserted
+   *                        into the events table.
+   */
   def insertData(parsedEvents: Seq[Event]): Int = {
-
     eventsTable.put(
       for(event <- parsedEvents) yield convert(event)
     )
-
     parsedEvents.length
   }
 }
