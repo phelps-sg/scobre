aef829a2fff6ffebacfade5e2946e813de8cf022
phelps-sg <sphelps@sphelps.net>
2018-03-18 12:48:18 +0000

    - code tidy

--- lse-data/src/main/scala/org/ccfea/tickdata/ReplayOrders.scala
+++ lse-data/src/main/scala/org/ccfea/tickdata/ReplayOrders.scala
@@ -1,18 +1,6 @@
 package org.ccfea.tickdata
 
-import org.ccfea.tickdata.collector.UnivariateTimeSeriesCollector
-import org.ccfea.tickdata.event.TickDataEvent
-import org.ccfea.tickdata.order.LimitOrder
-import org.ccfea.tickdata.order.offset.{MidPriceOffsetOrder, OppositeSideOffsetOrder, SameSideOffsetOrder}
-import org.ccfea.tickdata.storage.csv.UnivariateCsvDataCollator
-import org.ccfea.tickdata.storage.hbase.HBaseRetriever
-import org.ccfea.tickdata.storage.shuffled.{OffsettedTicks, RandomPermutation}
 import org.ccfea.tickdata.conf.ReplayerConf
-import org.ccfea.tickdata.simulator._
-import grizzled.slf4j.Logger
-import org.ccfea.tickdata.ui.OrderBookView
-
-import scala.swing.{Frame, SimpleSwingApplication}
 
 /**
  * The main application for running order-book reconstruction simulations.
@@ -20,8 +8,6 @@ import scala.swing.{Frame, SimpleSwingApplication}
  * (C) Steve Phelps 2016
  */
 object ReplayOrders {
-
-  val logger = Logger("org.ccfea.tickdata.OrderReplayer")
 
   def main(args: Array[String]) {
     val conf = new ReplayerConf(args)
